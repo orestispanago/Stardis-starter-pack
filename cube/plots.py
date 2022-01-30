707f9d2fa1b407f4774d52528b9632f18b6eb9dc
orestispanago <orestispanago@gmail.com>
2022-01-30 19:19:03 +0200

    Cube first run successful

--- cube/plots.py
+++ cube/plots.py
@@ -1,12 +1,14 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_csv("analytical_T.txt", names=["T"], sep=" ")
-df.plot()
-plt.show()
+# df = pd.read_csv("analytical_T.txt", names=["T"], sep=" ")
+# df.plot()
+# plt.show()
 
 
 results = pd.read_csv(
     "stardis_result_N10000.txt", index_col="#time", delim_whitespace=True
 )
 
+results["Temperature"].plot()
+plt.show()
